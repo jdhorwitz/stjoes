4aaab6b6b7f8a7a8263e4d9e7f5e8f4465088727
Josh Horwitz <joshua.d.horwitz@gmail.com>
2017-05-06 14:53:21 -0400

    Updated talks.js

--- src/common/talks.js
+++ src/common/talks.js
@@ -8,5 +8,130 @@ export const talks = [
     name: 'Amber',
     date: 'January 21, 2016',
     url: 'http://www.yahoo.com'
+  },
+  {
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
+  },{
+    name: 'Billy S',
+    date: 'March 5, 2017',
+    url: 'http://www.google.com'
   }
 ];
