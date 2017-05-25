f63e2cd6d3dc7ddb44c27c3cc3d300ec55528ef1
Josh Horwitz <joshua.d.horwitz@gmail.com>
2017-05-24 20:31:08 -0400

    Updated talks.js

--- src/common/talks.js
+++ src/common/talks.js
@@ -13,127 +13,158 @@ export const talks = [
     name: 'Rob S',
     date: 'March 2012',
     url: 'https://archive.org/details/RobS03172012'
-  },{
+  },
+  {
     name: 'Rebecca B',
     date: 'December 2010',
     url: 'https://archive.org/details/RebeccaB12182010'
-  },{
+  },
+  {
     name: 'Ross Z',
     date: 'September 2014',
     url: 'https://archive.org/details/RossZ09272014'
-  },{
+  },
+  {
     name: 'Christina N.',
     date: 'April 2017',
     url: 'https://archive.org/details/ChristinaN.April2017'
-  },{
+  },
+  {
     name: 'Matt B.',
     date: 'April 2017',
     url: 'https://archive.org/details/MattB'
-  },{
+  },
+  {
     name: 'Carol P',
     date: 'April 2017',
     url: 'https://archive.org/details/MattB'
-  },{
+  },
+  {
     name: 'Chrissy T',
     date: 'February 2014',
     url: 'https://archive.org/details/Chrissy02072015'
-  },{
+  },
+  {
     name: 'Micki M',
     date: 'June 2012',
     url: 'https://archive.org/details/MickiM06302012'
-  },{
+  },
+  {
     name: 'Ben R.',
     date: 'December 2012',
     url: 'https://archive.org/details/BenR120112'
-  },{
+  },
+  {
     name: 'Emily F.',
     date: 'February 2013',
     url: 'https://archive.org/details/EmilyF020913'
-  },{
+  },
+  {
     name: 'Kevin P.',
     date: 'March 5, 2017',
     url: 'https://archive.org/details/KevinP08302014'
-  },{
+  },
+  {
     name: 'Mark R.',
     date: 'September 2013',
     url: 'https://archive.org/details/MarkR09142013'
-  },{
+  },
+  {
     name: 'Carrie Jo.',
     date: 'October 2014',
     url: 'https://archive.org/details/CarrieJo10182014'
-  },{
+  },
+  {
     name: 'Amanda K',
     date: 'October 2014',
     url: 'https://archive.org/details/AmandaK10042014'
-  },{
+  },
+  {
     name: 'Pat G.',
     date: 'January 2008',
     url: 'https://archive.org/details/PatG01052008'
-  },{
+  },
+  {
     name: 'Larkin L.',
     date: 'October 2014',
     url: 'https://archive.org/details/LarkinL10252014m'
-  },{
+  },
+  {
     name: 'Adam H.',
     date: 'January 2015',
     url: 'https://archive.org/details/AdamH01032015'
-  },{
+  },
+  {
     name: 'Josh H.',
     date: 'December 2014',
     url: 'https://archive.org/details/JoshH12192014'
-  },{
+  },
+  {
     name: 'Pam H.',
     date: 'July 2010',
     url: 'https://archive.org/details/PamH07242010'
-  },{
+  },
+  {
     name: 'Dan T.',
     date: 'April 2012',
     url: 'https://archive.org/details/DanT04282012'
-  },{
+  },
+  {
     name: 'John Z.',
     date: 'March 2014',
     url: 'https://archive.org/details/JohnZ.32914'
-  },{
+  },
+  {
     name: 'Sarah O.',
     date: 'July 2015',
     url: 'https://archive.org/details/SarahO.72515'
-  },{
+  },
+  {
     name: 'Trevor S',
     date: 'March 2015',
     url: 'https://archive.org/details/TrevorS.31415'
-  },{
+  },
+  {
     name: 'Frank K.,
     date: 'January 2011',
     url: 'https://archive.org/details/FrankK011412'
-  },{
+  },
+  {
     name: 'Garrett G.',
     date: 'April 2014',
     url: 'https://archive.org/details/GarrettG.42614'
-  },{
+  },
+  {
     name: 'Lauren H.',
     date: 'March 2012',
     url: 'https://archive.org/details/LaurenH03242012'
-  },{
+  },
+  {
     name: 'Herb M.',
     date: 'February 2012',
     url: 'https://archive.org/details/HerbM020412'
-  },{
+  },
+  {
     name: 'Charles C',
     date: 'August 2012',
     url: 'https://archive.org/details/CharlesC08112012'
-  },{
+  },
+  {
     name: 'Molly',
     date: 'November 2014',
     url: 'https://archive.org/details/Molly11012014'
-  },{
+  },
+  {
     name: 'Dedra',
     date: 'August 2014',
     url: 'https://archive.org/details/Dedra08232014'
-  },{
+  },
+  {
     name: 'Alisha S',
     date: 'October 2012',
     url: 'https://archive.org/details/AlishaS10272012_734'
-  },{
+  },
+  {
     name: 'Marty G.',
     date: 'August 2015',
     url: 'https://archive.org/details/MartyG.815154'
